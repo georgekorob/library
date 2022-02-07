4ada9c5236c4297a16a281b7a36845733c1ab465
George <georgekorob@gmail.com>
2022-02-07 11:07:45 +0300

    AuthorItem

---
+++ frontend/src/components/Author.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const AuthorItem = ({author}) => {
+    return (
+        <tr>
+            <td>
+                {author.first_name}
+            </td>
+            <td>
+                {author.last_name}
+            </td>
+            <td>
+                {author.birthday_year}
+            </td>
+        </tr>
+    )
+}
