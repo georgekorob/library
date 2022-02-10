22b76987702570871eb578b370c66b4cff3f62ba
George <georgekorob@gmail.com>
2022-02-10 13:01:23 +0300

    urls

--- library/urls.py
+++ library/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from authors.views import AuthorModelViewSet
+from authors.views import AuthorModelViewSet, BookModelViewSet, BiographyModelViewSet
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
+router.register('book', BookModelViewSet)
+router.register('biography', BiographyModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
