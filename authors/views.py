44b15e150d82c6a6507a446e1f99e23110bc5101
George <georgekorob@gmail.com>
2022-02-10 12:58:10 +0300

    views

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Author
-from .serializers import AuthorModelSerializer
+from .models import Author, Book,Biography
+from .serializers import AuthorModelSerializer,BiographyModelSerializer,BookModelSerializer
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 
 
 # Create your views here.
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+
+
+class BiographyModelViewSet(ModelViewSet):
+    queryset = Biography.objects.all()
+    serializer_class = BiographyModelSerializer
+
+
+class BookModelViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
