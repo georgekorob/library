51ea92449b7f54f9a431b54437489af9a7695c76
George <georgekorob@gmail.com>
2022-02-19 14:38:15 +0300

    IsAuthenticated

--- authors/admin.py
+++ authors/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from authors.models import Author
+from authors.models import Author, Book, Biography
 
 # Register your models here.
 admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Biography)
--- authors/management/commands/fill_db.py
+++ authors/management/commands/fill_db.py
@@ -1,5 +1,7 @@
 import json
 import os
+
+from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from authors.models import Author, Biography, Book
@@ -16,9 +18,27 @@ def load_from_json(file_name):
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        Author.objects.all().delete()
-        Biography.objects.all().delete()
-        Book.objects.all().delete()
+        if os.path.exists('db.sqlite3'):
+            os.remove('db.sqlite3')
+        print('deleted db.sqlite3')
+        for dirapp in os.listdir():
+            if os.path.isdir(dirapp) and os.path.exists(f'{dirapp}/migrations'):
+                for filemig in os.listdir(f'{dirapp}/migrations'):
+                    if filemig not in ['__init__.py', '__pycache__']:
+                        filename = f'{dirapp}/migrations/{filemig}'
+                        os.remove(filename)
+                        print(f'deleted {filename}')
+        print('makemigrations...')
+        call_command('makemigrations')
+        print('migrate...')
+        call_command('migrate')
+        User.objects.create_superuser(username=os.getenv('SUPER_USER_USERNAME'),
+                                      password=os.getenv('SUPER_USER_PASSWORD'),
+                                      email=os.getenv('SUPER_USER_EMAIL'))
+        User.objects.create_user(username='geekbrains',
+                                 password='passfortest',
+                                 email='georgekorob@gmail.com')
+
         author1 = Author.objects.create(first_name='Александр', last_name='Грин', birth_year='1880')
         author2 = Author.objects.create(first_name='Александр', last_name='Пушкин', birth_year='1799')
         # authors = [author1, author2]
