cdf0df2147b2c0f42126abf32caf9f3c7ca7646e
George <georgekorob@gmail.com>
2022-03-06 14:58:48 +0300

    views

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.generics import ListAPIView
+from .serializers import UserSerializer, UserSerializerWithFullName
+
 
 # Create your views here.
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.request.version == 'v2':
+            return UserSerializerWithFullName
+        return UserSerializer
