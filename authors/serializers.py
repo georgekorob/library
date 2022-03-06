94850fe55f6fbc029bf0260abe5457491b02bf25
George <georgekorob@gmail.com>
2022-03-06 15:18:55 +0300

    serializers

--- authors/serializers.py
+++ authors/serializers.py
@@ -1,5 +1,4 @@
-from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer
 from .models import Author, Book, Biography
 
 
@@ -7,6 +6,12 @@ class AuthorModelSerializer(ModelSerializer):
     class Meta:
         model = Author
         fields = '__all__'
+
+
+class AuthorBaseSerializer(ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('first_name', 'birth_year')
 
 
 class BiographyModelSerializer(ModelSerializer):
@@ -20,6 +25,13 @@ class BiographyModelSerializer(ModelSerializer):
 class BookModelSerializer(ModelSerializer):
     # authors = serializers.StringRelatedField(many=True)
 
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class BookBaseSerializer(ModelSerializer):
+    # 10
     class Meta:
         model = Book
         fields = '__all__'
