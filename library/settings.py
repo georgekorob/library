6dc9c287d4ed9157be0f3a351852d2165d29b2ab
George <georgekorob@gmail.com>
2022-03-06 15:03:27 +0300

    UrlPathVersioning

--- library/settings.py
+++ library/settings.py
@@ -158,5 +158,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
 }
